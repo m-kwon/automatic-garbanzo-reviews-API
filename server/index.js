8d11dee17deb3b3108fa2761ea227148eae12ba2
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-08-11 16:06:14 -0700

    Initialized server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/', (req, res) => res.status(200).send('Welcome'));
+
 const port = 3001;
 const host = '0.0.0.0';
 app.listen(port, host, () => console.log(`Server has started on **http://${host}:${port}`));
+

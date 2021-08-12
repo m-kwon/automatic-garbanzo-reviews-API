cbae4965a45c71780fb81afa92efd0a18e41509c
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-08-11 19:58:18 -0700

    Further developed work environment

---
+++ server/database/example.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'port',
+  username: 'username',
+  password: 'password',
+  database: 'database name',
+  port: 5432
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./database');
 
 const app = express();
 
@@ -7,7 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// ROUTES
+// example
 app.use('/', (req, res) => res.status(200).send('Welcome'));
+
+// GET review data
+
+// GET review metaData
+
+// POST review
+
+// PUT helpful review
+
+// PUT report review
 
 const port = 3001;
 const host = '0.0.0.0';

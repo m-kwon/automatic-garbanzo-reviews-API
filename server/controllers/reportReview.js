9d1c29ac0152e1ac5e60e68e1e66b699276bec82
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-08-13 18:29:40 -0700

    Implemented PUT /review/:review_id/report handler

--- server/controllers/reportReview.js
+++ server/controllers/reportReview.js
@@ -1,3 +1,14 @@
-module.exports = (req, res) => {
-  res.status(200).send('hello');
-}
+const pool = require('../../database');
+
+module.exports = async (req, res) => {
+  let text = `
+    UPDATE reviews
+    SET reported = true
+    WHERE id = $1;
+  `;
+  let values = [req.params.review_id];
+
+  pool.query(text, values)
+    .then(() => res.status(200).send('Reported review'))
+    .catch((err) => res.status(500).send('Failed to report review'))
+};
